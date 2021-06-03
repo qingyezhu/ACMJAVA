28e41ff94eb6c4e07a90279ed16e12da7d04e7ee
wang.zhu <xiaoxian1369@126.com>
2021-06-03 15:47:54 +0800

    'update'

---
+++ src/com/wangzhu/Test.java
@@ -0,0 +1,10 @@
+package com.wangzhu;
+
+/**
+ * Created by wangz on 2021/6/3 15:41.
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试，hello");
+    }
+}
